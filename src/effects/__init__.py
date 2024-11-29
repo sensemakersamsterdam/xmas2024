cfc987ccb34df993870a11a2b8c5ad982811af26
Karijn <k.wessing@in2tech.nl>
2024-11-29 21:38:20 +0100

    2 New effects
    added fader to __init__.py

--- src/effects/__init__.py
+++ src/effects/__init__.py
@@ -436,3 +436,16 @@ def text2color(params, default_color_rgb=(255, 0, 0)):
         logging.exc(e, "Cannot convert color setting, setting to {default_color_rgb}}")
         color_rgb = default_color_rgb
     return color_rgb
+
+
+def fader(fade_index: int, fade_max : int, fade_from, fade_to):
+    if isinstance(fade_from, tuple) and isinstance(fade_to, tuple):
+        #print(f"fade tuples {fade_index}, {fade_max}, {fade_from}, {fade_to}")
+        return tuple([fader(fade_index, fade_max, fade_from[i], fade_to[i]) for i in range(min(len(fade_from), len(fade_to)))])
+    if isinstance(fade_from, list) and isinstance(fade_to, list):
+        #print(f"fade tuples {fade_index}, {fade_max}, {fade_from}, {fade_to}")
+        return [fader(fade_index, fade_max, fade_from[i], fade_to[i]) for i in range(min(len(fade_from), len(fade_to)))]  
+    else:
+        #print(f"fade floats {fade_index}, {fade_max}, {fade_from}, {fade_to}")
+        return round(((fade_from * (fade_max - fade_index)) + (fade_to * fade_index)) / fade_max)
+    
